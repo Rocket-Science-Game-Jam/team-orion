7481b86a9bfd42869e165583c26cf15e625a604b
RAPHAEL\Wolfsprinzessin <wolfs.prinzessin@yahoo.de>
2020-10-10 20:58:57 +0200

    Kamera Bewegung

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+    }
+}
