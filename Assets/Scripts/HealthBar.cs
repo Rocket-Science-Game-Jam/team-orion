3d574347bc514d9e4ad255033eaaa660849b3841
Thunder <stefano.gampe@googlemail.com>
2020-10-10 15:04:33 +0200

    add healthbar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    [SerializeField]
+    private Slider slider = null;
+    [SerializeField]
+    private  Gradient gradient = new Gradient();
+    [SerializeField]
+    private Image fill = null;
+
+    public void SetMaxHealth(int health) {
+        slider.maxValue = health;
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int health) {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private int health = 100;
+    [SerializeField]
+    private HealthBar healthbar = null;
+    private int score = 0;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    private void Start() {
+        if(healthbar != null) {
+            healthbar.SetMaxHealth(health);
+        } else {
+            Debug.LogWarning("Player: no healbar found");
+        }
+    }
+
+}
