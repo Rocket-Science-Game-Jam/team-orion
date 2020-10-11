32181f40f5c5648e1c0ca08c86661576057d2d9a
Thunder <stefano.gampe@googlemail.com>
2020-10-11 12:19:48 +0200

    add scene loader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    private static SceneLoader instance;
+
+    public static SceneLoader GetInstance() {
+        return instance;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        if(instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadSceneWithIndex(int index) {
+        SceneManager.LoadScene(index);
+    }
+
+}
