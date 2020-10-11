8606aba187755ef17579be2f24d3f140a8288176
Thunder <stefano.gampe@googlemail.com>
2020-10-11 02:09:45 +0200

    player animation

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+[Serializable]
+public class Boundary {
+    public float xMin, xMax, yMin, yMax;
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Animator), typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour {
 
-    public float moveSpeed = 10.0f;
-    public Rigidbody2D player;
+    [SerializeField]
+    private float moveSpeed = 10.0f;
+    [SerializeField]
+    private float tilt = 4;
+    [SerializeField]
+    private Boundary boundary = new Boundary();
 
-    // Start is called before the first frame update
-    void Start() {
-        player = this.GetComponent<Rigidbody2D>();        
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    private Vector2 playerInput = new Vector2();
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    private void Start() {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate() {
-        MovePlayer();        
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void FixedUpdate() {
+        MovePlayer();
     }
 
-    public void MovePlayer() {
-        player.velocity = new Vector2 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed;
+    private void Update() {
+        anim.SetFloat("VelocityX", playerInput.x);
+        anim.SetFloat("VelocityY", playerInput.y);
+    }
+
+    private void MovePlayer() {
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+
+        rb.velocity = playerInput * moveSpeed;
+
+        rb.position = new Vector2(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax)
+        );
     }
 
 }
