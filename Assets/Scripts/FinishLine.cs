b825447d6726e1944beceb8db410e284bcc874f5
Thunder <stefano.gampe@googlemail.com>
2020-10-11 12:35:04 +0200

    add finish line

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class FinishLine : MonoBehaviour {
+
+    [SerializeField]
+    private UnityEvent onTriggerEnter = new UnityEvent();
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        GameObject obj = collision.gameObject;
+        if (obj.tag == "Player") {
+            //obj.GetComponent<Player>().TakeDamage(100);
+            onTriggerEnter.Invoke();
+        }
+    }
+
+}
