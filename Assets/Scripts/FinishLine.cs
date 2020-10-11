fc25b36bd764de9cb9907eca62587113fceb6942
Thunder <stefano.gampe@googlemail.com>
2020-10-11 14:57:40 +0200

    bug fixes: finish line

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -9,7 +9,6 @@ public class FinishLine : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision) {
         GameObject obj = collision.gameObject;
         if (obj.tag == "Player") {
-            //obj.GetComponent<Player>().TakeDamage(100);
             onTriggerEnter.Invoke();
         }
     }
