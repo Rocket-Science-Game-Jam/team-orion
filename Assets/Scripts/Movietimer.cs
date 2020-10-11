a03be288b00a03ff4f08d48b443674dc8de283c4
christinakoch <63714678+christinakoch@users.noreply.github.com>
2020-10-11 14:06:48 +0200

    title

---
+++ Assets/Scripts/Movietimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movietimer : MonoBehaviour
+{
+    public float timer = 16f;
+    private float currenttime = 0f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currenttime<timer)
+        {
+            currenttime += Time.deltaTime;
+            Debug.Log("time: " + currenttime);
+        }
+        else {
+            Debug.Log("end");
+        }
+
+   
+        
+    }
+}
