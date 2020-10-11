2bbfe70c0abe3b7244f178e0416e30bfe8cbdc7d
Thunder <stefano.gampe@googlemail.com>
2020-10-11 15:19:54 +0200

    destroy player and load game over scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour {
 
@@ -10,6 +12,8 @@ public class Player : MonoBehaviour {
     private GameObject explosion = null;
     [SerializeField]
     private Boundary boundary = new Boundary();
+    [SerializeField]
+    private UnityEvent onPlayerDied = new UnityEvent();
 
     /// <summary>
     /// Start is called before the first frame update
@@ -23,14 +27,12 @@ public class Player : MonoBehaviour {
     }
 
     public void TakeDamage(int damage) {
-        GameObject instance = Instantiate(explosion);
-        instance.transform.position = new Vector3(
-            transform.position.x + Random.Range(boundary.xMin, boundary.xMax),
-            transform.position.y + Random.Range(boundary.yMin, boundary.yMax),
-            transform.position.z);
-        instance.transform.parent = transform;
+        InstantiateExplosion();
         health -= damage;
         healthbar.SetHealth(health);
+        if(health <= 0) {
+            StartCoroutine(playerDied());
+        }
     }
 
     public void AddHealth(int value) {
@@ -38,6 +40,25 @@ public class Player : MonoBehaviour {
         if(health > healthbar.GetMaxHealth()) {
             health = healthbar.GetMaxHealth();
         }
+    }
+
+    private IEnumerator playerDied() {
+        Destroy(GetComponent<SpriteRenderer>());
+        Destroy(transform.GetChild(0).GetComponent<SpriteRenderer>());
+        for (int i = 0; i < 10; i++) {
+            InstantiateExplosion();
+        }
+        yield return new WaitForSeconds(1f);
+        onPlayerDied.Invoke();
+    }
+
+    private void InstantiateExplosion() {
+        GameObject instance = Instantiate(explosion);
+        instance.transform.position = new Vector3(
+            transform.position.x + Random.Range(boundary.xMin, boundary.xMax),
+            transform.position.y + Random.Range(boundary.yMin, boundary.yMax),
+            transform.position.z);
+        instance.transform.parent = transform;
     }
 
 }
